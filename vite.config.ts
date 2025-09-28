bb37ab44bad821aa93a147ccb1d2a105b6ceedd3
eslam120349 <mreslam249@gmail.com>
2025-09-28 13:03:28 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  base: "/", // ده مهم عشان Vercel يعرف يبدأ من الروت
 });
